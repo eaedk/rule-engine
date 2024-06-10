40f0a6d9ba5636563274457fca5a4fc22523d288
Emmanuel KOUPOH <emmanuelkoupoh@gmail.com>
2024-06-10 17:12:18 +0000

    logging

--- rule_engine_api/app/api/v0/endpoints/rules.py
+++ rule_engine_api/app/api/v0/endpoints/rules.py
@@ -1,11 +1,19 @@
-# app/api/v0/endpoints/rules.py
-
 from typing import List, Union
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.db.session import SessionLocal
 from app.db import crud
 from app.schemas.rule import Rule, RuleCreate, RuleUpdate
+import logging
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,  # Set the logging level to INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",  # Define the log format
+)
+
+# Create a logger
+logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
@@ -32,10 +40,15 @@ def create_new_rule(
     Returns:
         Union[Rule, List[Rule]]: The created rule(s).
     """
+    logger.info("Creating new rule(s)")
     if isinstance(rules, list):
-        return crud.create_multiple_rules(db, rules)
+        created_rules = crud.create_multiple_rules(db, rules)
+        logger.info("Multiple rules created successfully")
+        return created_rules
     else:
-        return crud.create_rule(db, rules)
+        created_rule = crud.create_rule(db, rules)
+        logger.info("Rule created successfully")
+        return created_rule
 
 
 @router.get("/{rule_id}", response_model=Rule)
@@ -50,6 +63,7 @@ def read_rule(rule_id: int, db: Session = Depends(get_db)):
     Returns:
         Rule: The rule with the specified ID.
     """
+    logger.info("Reading rule with ID: %s", rule_id)
     db_rule = crud.get_rule(db, rule_id)
     if db_rule is None:
         raise HTTPException(status_code=404, detail="Rule not found")
@@ -58,21 +72,26 @@ def read_rule(rule_id: int, db: Session = Depends(get_db)):
 
 @router.get("/", response_model=List[Rule])
 def read_rules(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    logger.info("Reading rules with skip=%s and limit=%s", skip, limit)
     rules = crud.get_rules(db, skip=skip, limit=limit)
     return rules
 
 
 @router.put("/{rule_id}", response_model=Rule)
 def update_existing_rule(rule_id: int, rule: RuleUpdate, db: Session = Depends(get_db)):
+    logger.info("Updating rule with ID: %s", rule_id)
     db_rule = crud.update_rule(db, rule_id, rule)
     if db_rule is None:
         raise HTTPException(status_code=404, detail="Rule not found")
+    logger.info("Rule updated successfully")
     return db_rule
 
 
 @router.delete("/{rule_id}", response_model=Rule)
 def delete_existing_rule(rule_id: int, db: Session = Depends(get_db)):
+    logger.info("Deleting rule with ID: %s", rule_id)
     db_rule = crud.delete_rule(db, rule_id)
     if db_rule is None:
         raise HTTPException(status_code=404, detail="Rule not found")
+    logger.info("Rule deleted successfully")
     return db_rule
--- rule_engine_api/app/api/v0/endpoints/transaction.py
+++ rule_engine_api/app/api/v0/endpoints/transaction.py
@@ -1,5 +1,4 @@
-# app/api/v0/endpoints/transaction.py
-
+import logging
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.db.session import SessionLocal
@@ -7,6 +6,15 @@ from app.db import crud
 from app.schemas.transaction import TransactionCreate
 from app.services.rule_engine import apply_rules
 from app.responses import StandardResponse
+import asyncio
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,  # Set the logging level to INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",  # Define the log format
+)
+# Create a logger
+logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
@@ -20,7 +28,9 @@ def get_db():
 
 
 @router.post("/check-transaction", response_model=StandardResponse)
-def check_transaction(transaction: TransactionCreate, db: Session = Depends(get_db)):
+async def check_transaction(
+    transaction: TransactionCreate, db: Session = Depends(get_db)
+):
     """
     Check a transaction against rules and return approval status.
 
@@ -33,13 +43,19 @@ def check_transaction(transaction: TransactionCreate, db: Session = Depends(get_
     """
     rules = crud.get_rules(db)
     transaction_dict = transaction.dict()
-    check = apply_rules(transaction_dict, rules)
+
+    # Async rule evaluation
+    loop = asyncio.get_event_loop()
+    check = await loop.run_in_executor(None, apply_rules, transaction_dict, rules)
+
     if check["has_succeeded"]:
+        logger.info("Transaction approved: %s", transaction)
         return StandardResponse(
             status="approved", status_code=200, message="Transaction approved"
         )
     else:
-        rejection_message = f"Transaction rejected:{check['message']}"
+        rejection_message = f"Transaction rejected: {check['message']}"
+        logger.error("Transaction rejected: %s", transaction)
         return StandardResponse(
             status="rejected", status_code=400, message=rejection_message
         )
@@ -47,5 +63,16 @@ def check_transaction(transaction: TransactionCreate, db: Session = Depends(get_
 
 @router.post("/save-transaction")
 def save_transaction(transaction: TransactionCreate, db: Session = Depends(get_db)):
+    """
+    Save a transaction to the database.
+
+    Args:
+        transaction (TransactionCreate): The transaction to save.
+        db (Session, optional): SQLAlchemy database session. Defaults to Depends(get_db).
+
+    Returns:
+        TransactionCreate: The saved transaction.
+    """
     db_transaction = crud.create_transaction(db, transaction)
+    logger.info("Transaction saved: %s", db_transaction)
     return db_transaction
