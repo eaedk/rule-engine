3aa05e715dead36b59bed4fadd2ea39f9eb239eb
Emmanuel KOUPOH <emmanuelkoupoh@gmail.com>
2024-06-11 09:30:35 +0000

    documentation and cleaning

--- rule_engine_api/app/main.py
+++ rule_engine_api/app/main.py
@@ -1,13 +1,3 @@
-# from fastapi import FastAPI
-# from app.api.v0 import endpoints
-# from app.db.session import engine
-# from app.db.models import Base
-
-# Base.metadata.create_all(bind=engine)
-
-# app = FastAPI()
-
-# app.include_router(endpoints.router, prefix="/v0")
 # app/main.py
 
 from fastapi import FastAPI
@@ -15,8 +5,11 @@ from app.api.v0.endpoints import api_router
 from app.db.session import engine
 from app.db.models import Base
 
+# Create all database tables based on the models defined
 Base.metadata.create_all(bind=engine)
 
+# Initialize the FastAPI application
 app = FastAPI()
 
+# Include the API router with a prefix for versioning
 app.include_router(api_router, prefix="/v0")
