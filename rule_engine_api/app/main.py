c14ecee3aef4c09b15215194713eff72044f3517
Emmanuel KOUPOH <emmanuelkoupoh@gmail.com>
2024-06-11 09:49:48 +0000

    filename specified and fixed

--- rule_engine_api/app/main.py
+++ rule_engine_api/app/main.py
@@ -1,10 +1,11 @@
 # app/main.py
 
+import os
 from fastapi import FastAPI
 from app.api.v0.endpoints import api_router
 from app.db.session import engine
 from app.db.models import Base
-from app.initial_data.insert_rules import insert_initial_rules
+from app.initial_data.insert_rules import insert_initial_rules, read_rules_from_json
 
 # Create all database tables based on the models defined
 Base.metadata.create_all(bind=engine)
@@ -15,5 +16,11 @@ app = FastAPI()
 # Include the API router with a prefix for versioning
 app.include_router(api_router, prefix="/v0")
 
+# Define the path to the JSON file containing initial rules
+initial_rules_file = os.path.join(
+    os.path.dirname(__file__), "initial_data", "initial_rules.json"
+)
+
 # Insert initial rules into the database
-insert_initial_rules()
+rules = read_rules_from_json(initial_rules_file)
+insert_initial_rules(rules)
